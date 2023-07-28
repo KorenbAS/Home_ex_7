1db1ed248c3a4f0b6b32514d6013fb9192b9e7de
KorenbAS78 <132934909+KorenbAS@users.noreply.github.com>
2023-07-28 11:02:04 +0300

    Update main.py

--- clean_folder/main.py
+++ clean_folder/main.py
@@ -67,14 +67,9 @@ def main(folder: Path):
 
 
 def start():
-    if sys.argv[1]:
+    if len(sys.argv) == 1:
         folder_for_scan = Path(sys.argv[1])
         print(f'Start in folder {folder_for_scan.resolve()}')
         main(folder_for_scan.resolve())
 
-if __name__ == "__main__":
-    if sys.argv[1]:
-        folder_for_scan = Path(sys.argv[1])
-        print(f'Start in folder: {folder_for_scan.resolve()}')
-        main(folder_for_scan.resolve())
 
