b0ec2312bec2fec5e562a28a7284a9f3d018791b
KorenbAS78 <132934909+KorenbAS@users.noreply.github.com>
2023-08-02 11:52:16 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(name='clean_folder',
       version='0.0.1',
-      description='Clean Folder',
+      description='Clean Folder_kas',
       author='Andrii Korniev',
       author_email='KorenbAS78@gmail.com',
       packages=find_namespace_packages(),
